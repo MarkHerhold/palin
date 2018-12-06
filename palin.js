8e5c939be2615918b71ddfa02b93c9d899796a76
范文杰 <wenjie.fan@malong.com>
2018-12-06 12:27:02 +0800

    add option for detect if need to show log source

--- palin.js
+++ palin.js
@@ -87,6 +87,7 @@ var formatter = function formatter(options, severity, date, elems) {
     */
     const indent = options.indent || defaultIndent;
     const objectDepth = options.objectDepth;
+    const displayCodePlace = options.hasOwnProperty('displayCodePlace')? options.displayCodePlace : true;
     const timestamp = (function () {
         if (check.function(options.timestamp)) {
             return options.timestamp; // user-provided timestamp generating function
@@ -145,8 +146,10 @@ var formatter = function formatter(options, severity, date, elems) {
 
             // add on the file and line number, which always go after the title, inline
             if (aggObj.file && aggObj.line) {
-                aggObj.file = truncFilename(aggObj.file, rootFolderName);
-                build += chalk.dim(` (${aggObj.file}:${aggObj.line})`);
+                if(displayCodePlace){
+	                aggObj.file = truncFilename(aggObj.file, rootFolderName);
+	                build += chalk.dim(` (${aggObj.file}:${aggObj.line})`);
+                }
                 delete aggObj.file;
                 delete aggObj.line;
             }
