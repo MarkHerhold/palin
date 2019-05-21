98d3139f3085b2ff1f72a0aad89a3d08040f0f61
Mark Herhold <mark.herhold@gmail.com>
2019-05-21 01:04:14 +0000

    rename variable and test

--- palin.js
+++ palin.js
@@ -87,7 +87,7 @@ const formatter = function formatter(options, severity, date, elems) {
     */
     const indent = options.indent || defaultIndent;
     const objectDepth = options.objectDepth;
-    const displayCodePlace = options.hasOwnProperty('displayCodePlace')? options.displayCodePlace : true;
+    const source = options.hasOwnProperty('source') ? options.source : true;
     const timestamp = (function () {
         if (check.function(options.timestamp)) {
             return options.timestamp; // user-provided timestamp generating function
@@ -146,7 +146,7 @@ const formatter = function formatter(options, severity, date, elems) {
 
             // add on the file and line number, which always go after the title, inline
             if (aggObj.file && aggObj.line) {
-                if(displayCodePlace){
+                if (source){
                     aggObj.file = truncFilename(aggObj.file, rootFolderName);
                     build += chalk.dim(` (${aggObj.file}:${aggObj.line})`);
                 }
